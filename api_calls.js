271ddaff0b7d9fefd5f980b6e9a40a4fb4a30a13
Marceliwut <mmirzynski@gmail.com>
2018-11-17 15:32:53 +0100

    Post request added

--- api_calls.js
+++ api_calls.js
@@ -6,3 +6,14 @@ exports.httpGet = function(theUrl)
     xmlHttp.send( null );
     return JSON.parse(xmlHttp.responseText)
 }
+
+exports.httpPost = function(theUrl, body)
+{
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "POST", theUrl, false ); // false for synchronous request
+	xmlHttp.setRequestHeader("Content-Type", "application/json");
+	xmlHttp.send(JSON.stringify({title:"Obojetne", body:"Hello world", userId: 1}));
+    return JSON.parse(xmlHttp.responseText)
+}
+
+
--- test/api_calls__tests.js
+++ test/api_calls__tests.js
@@ -5,4 +5,7 @@ describe('Simple tests on API calls', function () {
  it('should return toDoCompleted status correctly toDo[1] == false', function () {
         assert.equal(api.httpGet('https://jsonplaceholder.typicode.com/todos/1')['completed'], false);
     });
+ it('should return newPostDetails', function () {
+		assert.equal(api.httpPost('https://jsonplaceholder.typicode.com/posts/')['title'], "Obojetne");
+	});
 });
