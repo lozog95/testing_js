1c6d600535a8c6e2b4cbb8dd15a90fa9b3e1297d
lozog95 <lozog95@gmail.com>
2018-11-17 15:51:11 +0100

    chai-as-promised added

--- test/api_calls__tests.js
+++ test/api_calls__tests.js
@@ -1,5 +1,10 @@
 var api = require('../api_calls.js');
 var assert = require('assert');
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
 
 describe('Simple tests on API calls', function () {
  it('should return toDoCompleted status correctly toDo[1] == false', function () {
@@ -8,4 +13,8 @@ describe('Simple tests on API calls', function () {
  it('should return newPostDetails', function () {
 		assert.equal(api.httpPost('https://jsonplaceholder.typicode.com/posts/')['title'], "Obojetne");
 	});
+it('should return details with promises', function () {
+		return expect(Promise.resolve(api.httpPost('https://jsonplaceholder.typicode.com/posts/')).should.eventually.have.property('title'));
+});
+
 });
